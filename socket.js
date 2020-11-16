202a9c66a072497561c26ea9d272663fb188a0ba
kevkaka25 <kkmbogo@wpi.edu>
2020-11-15 19:48:14 -0500

    Latest update

--- socket.js
+++ socket.js
@@ -4,47 +4,79 @@ const { parse } = require('json2csv');
 var HOST = '0.0.0.0';//(not sure of host)
 var PORT = 5000;// listens on specified port 5000
 
+
+function textToBin(text) {
+    var length = text.length,
+        output = [];
+    for (var i = 0;i < length; i++) {
+        var bin = text[i].charCodeAt().toString(2);
+        output.push(Array(6-bin.length+1).join("0") + bin)
+    }
+    return output.join("")
+}
+function bintoarr(val){
+    const newArr = [];
+    while(val.length%32==0) newArr.push(arr.splice(0,2));
+    return newArr;
+}
+
+let bindata = '';
+
 net.createServer(function(sock) {//creates a server with a listener for a socket
     console.log('CONNECTED:',sock.remoteAddress,':',sock.remotePort);// prints socket info
-    // sock.setEncoding("utf-8"); //set data encoding (either 'ascii', 'utf8', or 'base64')
+     sock.setEncoding("ascii"); //set data encoding (either 'ascii', 'utf8', or 'base64')
     sock.on('data', function(data) {// event name  of data,  listens for data
         console.log('DATA',sock.remoteAddress,': ',data,typeof data,"===",typeof "exit");
-        console.log("======================")
-        console.log(data)
-        console.log("======================2")
-            let buff = new Buffer(data, 'base64');
-            let text = buff.toString('ascii');
 
-        //const number = data.indexOf("{");
-        //const string = data.substring(number);
-        const JSONfile = JSON.parse(text);
-
-        if(data === "exit") console.log('exit message received !');
-
-        console.log(JSONfile)
-        console.log("==========3")
-        const fields = [ 'payload' ];
-        const opts = { fields };
-        const fields2= ['rx_time','rx_sample','cplx']
-        const opts2={fields2}
-
-        try {
-            const csv = parse(JSONfile, opts);
-            const csv2 = parse(JSONfile.metadata, opts2);
-
-            fs.writeFileSync('data1.csv', csv)
-            fs.writeFileSync('metadata.csv',csv2)
-        } catch (err) {
-            console.error(err);
-        }
-
-
-
-
-
+        const number = data.indexOf("{");
+        const string = data.substring(number);
+        const Jsonobg= JSON.parse(string)
+        console.log(string);
+        console.log("======================3")
+        const  payloadData = Jsonobg.payload
+        const val = payloadData.toString(2);
+        console.log("======================33")
+        console.log(val)
+        console.log("======================33")
+        const bin = textToBin(payloadData)
+        bindata = arrayToCSV(Arraycreator(bin))
+        console.log(bindata);
+        fs.writeFile('data.csv', bindata, function (err) {
+            if (err) return console.log(err);
+        });
+        console.log("======================4")
+        const darr = bintoarr(bin)
+        console.log(darr)
     });
 
 
 }).listen(PORT, HOST, function() {
     console.log("server accepting connections");
 });
+
+function Arraycreator(byte){
+    let bigarr = [];
+    let iterator = byte.split('');
+    let arr = [];
+    let chunk = [];
+    let char = [];
+    while (iterator.length > 0) {
+        chunk = iterator.splice(0,32);
+        let str = chunk.toString();
+        let blac = str.replace(/,/g, "");
+        arr.push(blac);
+    }
+    while (arr.length > 0) {
+        char = arr.splice(0,2);
+        bigarr.push(char);
+    }
+    return bigarr
+}
+
+const arrayToCSV = (arr, delimiter = ',') =>
+    arr
+        .map(v =>
+            v.map(x => (isNaN(x) ? `"${x.replace(/"/g, '""')}"` : x)).join(delimiter)
+        )
+        .join('\n');
+
