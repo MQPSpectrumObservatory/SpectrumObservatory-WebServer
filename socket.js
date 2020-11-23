878cb252bf69e3539c71442408b2d3455f3632bf
kevkaka25 <kkmbogo@wpi.edu>
2020-11-22 19:02:55 -0500

    Latest update

--- socket.js
+++ socket.js
@@ -3,56 +3,7 @@ var fs = require('fs');
 const { parse } = require('json2csv');
 var HOST = '0.0.0.0';//(not sure of host)
 var PORT = 5000;// listens on specified port 5000
-
-
-function textToBin(text) {
-    var length = text.length,
-        output = [];
-    for (var i = 0;i < length; i++) {
-        var bin = text[i].charCodeAt().toString(2);
-        output.push(Array(6-bin.length+1).join("0") + bin)
-    }
-    return output.join("")
-}
-function bintoarr(val){
-    const newArr = [];
-    while(val.length%32==0) newArr.push(arr.splice(0,2));
-    return newArr;
-}
-
-let bindata = '';
-
-net.createServer(function(sock) {//creates a server with a listener for a socket
-    console.log('CONNECTED:',sock.remoteAddress,':',sock.remotePort);// prints socket info
-     sock.setEncoding("ascii"); //set data encoding (either 'ascii', 'utf8', or 'base64')
-    sock.on('data', function(data) {// event name  of data,  listens for data
-        console.log('DATA',sock.remoteAddress,': ',data,typeof data,"===",typeof "exit");
-
-        const number = data.indexOf("{");
-        const string = data.substring(number);
-        const Jsonobg= JSON.parse(string)
-        console.log(string);
-        console.log("======================3")
-        const  payloadData = Jsonobg.payload
-        const val = payloadData.toString(2);
-        console.log("======================33")
-        console.log(val)
-        console.log("======================33")
-        const bin = textToBin(payloadData)
-        bindata = arrayToCSV(Arraycreator(bin))
-        console.log(bindata);
-        fs.writeFile('data.csv', bindata, function (err) {
-            if (err) return console.log(err);
-        });
-        console.log("======================4")
-        const darr = bintoarr(bin)
-        console.log(darr)
-    });
-
-
-}).listen(PORT, HOST, function() {
-    console.log("server accepting connections");
-});
+const buffSize= Buffer.alloc(4096);
 
 function Arraycreator(byte){
     let bigarr = [];
@@ -80,3 +31,66 @@ const arrayToCSV = (arr, delimiter = ',') =>
         )
         .join('\n');
 
+function textToBin(text) {
+    var length = text.length,
+        output = [];
+    for (var i = 0;i < length; i++) {
+        var bin = text[i].charCodeAt().toString(2);
+        output.push(Array(6-bin.length+1).join("0") + bin)
+    }
+    return output.join("")
+}
+
+
+let bindata = '';
+
+net.createServer(function(sock) {//creates a server with a listener for a socket
+    console.log('CONNECTED:',sock.remoteAddress,':',sock.remotePort);
+    // prints socket info
+    var buffers = []
+    sock.setEncoding("ascii"); //set data encoding (either 'ascii', 'utf8', or 'base64')
+    sock.on('data', function(incomingData) {// event name  of data,  listens for data
+        console.log(sock.bytesRead+ " bytes");
+        // console.log(incomingData)
+        buffers.push(incomingData)
+        console.log("receiving data batches...")
+        // console.log(incomingData,sock.remoteAddress,': ',data,typeof data,"===",typeof "exit");
+
+    }).on('end', function(){
+        console.log("finished receiving all batches");
+        console.log(sock.bytesRead+ " final bytes");
+        var stringData = buffers.join("").toString();
+
+        console.log("extracting payload data...")
+
+        // reset buffers array for next request
+        buffers = []
+
+        const number = stringData.indexOf("{");
+        stringData = stringData.substring(number);
+        var jsonData = JSON.parse(stringData);
+        // console.log(jsonData);
+
+        const  payloadData = jsonData.payload;
+
+        console.log("converting payload to binary data")
+
+        const bin = textToBin(payloadData);
+
+        console.log("converting binary data to CSV format...")
+
+        bindata = arrayToCSV(Arraycreator(bin));
+
+        console.log("writing CSV file...")
+
+        fs.writeFile('data.csv', bindata, function (err) {
+            if (err) return console.log(err);
+        });
+
+        console.log("CSV writing complete!");
+    });
+
+
+}).listen(PORT, HOST, function() {
+    console.log("server accepting connections");
+});
